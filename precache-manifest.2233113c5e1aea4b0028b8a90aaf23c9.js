7146f8a95ed486ba0888f04736f800601e7fed78
Aleksandr Solntsev <san2008nas@gmail.com>
2023-04-05 16:31:57 +0300

    Updates

---
+++ precache-manifest.2233113c5e1aea4b0028b8a90aaf23c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "640115b16efb04293118",
+    "url": "/aleksandr_solntsev_cards/static/css/main.9cc78fd6.chunk.css"
+  },
+  {
+    "revision": "640115b16efb04293118",
+    "url": "/aleksandr_solntsev_cards/static/js/main.640115b1.chunk.js"
+  },
+  {
+    "revision": "990ec6b76a32fa790a2c",
+    "url": "/aleksandr_solntsev_cards/static/js/1.990ec6b7.chunk.js"
+  },
+  {
+    "revision": "cf1d70357a3501de71a6",
+    "url": "/aleksandr_solntsev_cards/static/js/runtime~main.cf1d7035.js"
+  },
+  {
+    "revision": "3990442bb9cdd7027dab6548ff9102f7",
+    "url": "/aleksandr_solntsev_cards/static/media/pngwing.com.3990442b.png"
+  },
+  {
+    "revision": "4b07f8240233f8429f095532a78f137c",
+    "url": "/aleksandr_solntsev_cards/index.html"
+  }
+];
